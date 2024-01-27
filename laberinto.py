e46126804423665d7755197905f10984fa2c26cf
edujoo <teamofstrike@gmail.com>
2024-01-26 21:05:09 -0600

    Primera parte proyecto intregador

---
+++ laberinto.py
@@ -0,0 +1,5 @@
+# Solicitar al usuario que ingrese un nombre
+nombre_jugador = input("Ingresa un nombre: ")
+
+# Imprime un mensaje de bienvenida con el nombre del jugador
+print(f"Muchas gracias, {nombre_jugador}! Llega hasta el final del laberinto.")
